c5401b621ee42427229f0307a7cf5a7f503f18b6
barry-gallagher <barry.gallagher@noaa.gov>
2024-10-26 19:32:57 -0400

    minor changes for nbs06

--- scripts/refactor_combine_spec.py
+++ scripts/refactor_combine_spec.py
@@ -5,8 +5,8 @@ from nbs import configs
 from nbs.bruty import nbs_postgres
 from nbs.bruty.world_raster_database import WorldDatabase, use_locks, UTMTileBackendExactRes, NO_OVERRIDE
 
-config = configs.read_config(r"C:\Git_Repos\Bruty\nbs\scripts\barry.gallagher\debug.config", log_files=False,
-                             base_config_path = r"C:\Git_Repos\Bruty\nbs\scripts\base_configs")['DEFAULT']
+config = configs.read_config(r"D:\git_repos\bruty_dev_debugging\nbs\scripts\Barry.Gallagher.LA\bg_dbg.config", log_files=False,
+                             base_config_path=r"D:\git_repos\bruty_dev_debugging\nbs\scripts\base_configs")['DEFAULT']
 conn_info = nbs_postgres.connect_params_from_config(config)
 conn_info.database = "tile_specifications"
 conn_info.tablenames = ['combine_spec_tiles']
@@ -32,7 +32,9 @@ def fill_spec_tables_from_disk():
     #  WHERE production_branch='PBC' and tile=1 and resolution=4
     cursor.execute(f"""select production_branch, locality, utm, hemisphere, datum, tile, resolution, datatype, for_navigation, c_id, res_id, tile_id from view_individual_combines""")
     tiles = cursor.fetchall()
-    for (production_branch, locality, utm, hemisphere, datum, tile, resolution, datatype, for_nav, c_id, r_id, t_id) in tiles:
+    for i, (production_branch, locality, utm, hemisphere, datum, tile, resolution, datatype, for_nav, c_id, r_id, t_id) in enumerate(tiles):
+        if i % 100 == 0:
+            print(f"{i} of {len(tiles)}")
         world_db_path = fr"X:\bruty_databases\{production_branch}_{locality}_utm{utm}{hemisphere}_{datum}_Tile{tile}_res{int(resolution)}_{datatype}{'_not_for_navigation'*(not for_nav)}"
         try:
             db = WorldDatabase.open(world_db_path)
