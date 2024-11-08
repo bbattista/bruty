9cf5f74b7c11228cec2939aaead227114849e175
Barry Gallagher <Barry.Gallagher@nos.noaa>
2024-11-08 12:04:57 -0500

    revising postgres -- Adding a view and adding some of the logic from the qgis symbology to the views

--- scripts/tile_specifications_scripts.sql
+++ scripts/tile_specifications_scripts.sql
@@ -1,3 +1,13 @@
+-- Useful queries:
+-- Show the running combines
+SELECT * FROM (SELECT c_id, (SELECT c_id NOT IN (SELECT c_id FROM spec_combines FOR UPDATE SKIP LOCKED)) AS running from view_individual_combines) as a WHERE a.running=True
+-- See how many combines are running for a tile
+SELECT count(*) FROM (SELECT (SELECT res_id NOT IN (SELECT r_id FROM spec_resolutions FOR UPDATE SKIP LOCKED)) AS running from view_individual_combines WHERE tile_id=1256) as a WHERE a.running=True
+
+
+
+
+
 -- Create the primary tile table which holds the review tile geometry and production_branch, locality, hemisphere, utm zone, datum, and other parameters
 CREATE TABLE IF NOT EXISTS public.spec_tiles
 (
@@ -235,7 +245,8 @@ CREATE OR REPLACE TRIGGER edit_resolutions
 
 
 
--- Primary view for the burty combines
+-- Primary view for the bruty combines
+DROP VIEW IF EXISTS view_tiles;
 DROP VIEW IF EXISTS view_individual_combines;
 CREATE VIEW view_individual_combines AS
 	-- QGIS likes a key as the first column unless you used "setKeyColumn" in api so add it first (as a different name to avoid conflict)
@@ -244,7 +255,13 @@ CREATE VIEW view_individual_combines AS
 		TI.request_export, TI.export_request_time,R.export_running,
 		TI.production_branch, TI.utm, TI.hemisphere, TI.tile, TI.datum, TI.locality,
 		R.resolution, R.closing_distance, B.datatype, B.for_navigation,
-		TI.priority, B.combine_start_time, B.combine_end_time,
+		TI.priority,
+		((datatype<>'enc' and (combine_request_time>combine_start_time OR combine_request_time>combine_end_time OR (combine_request_time IS NOT NULL AND (combine_start_time IS NULL OR combine_end_time IS NULL)))) OR
+		(datatype='enc' and (enc_request_time>combine_start_time OR enc_request_time>combine_end_time OR (enc_request_time IS NOT NULL AND (combine_start_time IS NULL OR combine_end_time IS NULL))))) combine_waiting,
+		(TI.export_request_time>R.export_start_time OR TI.export_request_time>R.export_end_time OR (TI.export_request_time IS NOT NULL AND (R.export_start_time IS NULL OR R.export_end_time IS NULL))) export_waiting,
+		(R.export_end_time<B.combine_start_time OR R.export_end_time<B.combine_end_time OR (R.export_end_time IS NULL AND (B.combine_end_time IS NOT NULL))) combined_after_export,
+		(export_end_time>combine_end_time AND export_end_time>combine_start_time AND export_code<=0) exported,
+		B.combine_start_time, B.combine_end_time,
 		B.combine_code, B.combine_tries, B.combine_data_location, B.combine_warnings_log, B.combine_info_log,
 		R.export_start_time, R.export_end_time,
 		R.export_code, R.export_tries, R.export_warnings_log, R.export_info_log, R.export_data_location,
@@ -332,68 +349,40 @@ CREATE OR REPLACE TRIGGER edit_combine_view_trigger
 
 
 
--- Obsolete view -- just using individual combines now
-DROP VIEW IF EXISTS view_individual_resolutions;
-CREATE or REPLACE VIEW view_individual_resolutions as
-	-- QGIS likes a key as the first column unless you used "setKeyColumn" in api so add it first (as a different name to avoid conflict)
-	-- Also I want to remove the buffered_geometry (makes QGIS really slow) so have to manually add the spec_resolutions columns
-	SELECT r_id as res_id, TI.request_export, TI.export_request_time, R.export_running,
-		TI.production_branch, TI.utm, TI.hemisphere, TI.tile, TI.datum, TI.locality,
-		R.resolution, R.closing_distance, R.export_start_time, R.export_end_time,
-		R.export_code, R.export_tries, R.export_warnings_log, R.export_info_log,
-		R.tile_id, TI.geometry  -- exit_code, tries, end_time, start_time, resolution, closing_distance, r_id, TI.*
-	FROM spec_resolutions R JOIN spec_tiles TI ON (R.tile_id = TI.t_id);
 
+DROP VIEW IF EXISTS view_tiles;
+CREATE or REPLACE VIEW view_tiles as
+--CONCAT('Tile ',tile,' ', resolution,'m ', datum, ' ', production_branch, '_', utm, hemisphere, ' ', locality) tile_name,
+SELECT tile_id, production_branch, utm, tile, datum, hemisphere, locality, priority,
+	sum((combine_running)::int) combining,
+	sum((datatype<>'enc' and combine_waiting)::int) combine_waiting,
+	sum((datatype='enc' and combine_waiting)::int) enc_combine_waiting,
+	bool_or(combine_code IS NULL) never_combined,
+	bool_or(combine_code>0) combine_errors,
 
-CREATE OR REPLACE FUNCTION edit_resolution_view()
-RETURNS TRIGGER
-LANGUAGE plpgsql
-AS $function$
-BEGIN
-	IF TG_OP = 'UPDATE' THEN
-		UPDATE spec_resolutions SET export_start_time=NEW.export_start_time, export_end_time=NEW.export_end_time, export_code=NEW.export_code,
-			export_info_log=NEW.export_info_log, export_warnings_log=NEW.export_warnings_log,
-			export_tries=NEW.export_tries, export_data_location=NEW.export_data_location
-			WHERE r_id=NEW.res_id;
-		IF NEW.request_export = True THEN
-			UPDATE spec_tiles SET request_export=TRUE WHERE t_id = OLD.tile_id;  -- (select tile_id from spec_resolutions WHERE r_id=OLD.tile_id);
-		END IF;
-		RETURN NEW;
-	ELSIF TG_OP = 'DELETE' THEN
-		DELETE FROM spec_resolutions WHERE r_id=OLD.res_id;
-		RETURN NULL;
-	END IF;
-	RETURN NEW;
-END;
-$function$;
+	sum((export_running)::int) exporting,
+	sum((datatype<>'enc' and export_waiting)::int) export_waiting,
+	sum((datatype='enc' and combined_after_export)::int) enc_newer_than_export,
+	sum((datatype<>'enc' and combined_after_export)::int) combine_newer_than_export,
+	sum((datatype='enc' and export_waiting)::int) enc_export_waiting,
+	bool(sum((NOT(datatype='enc' OR (datatype<>'enc' and exported)))::int)=0) export_complete,
+	bool_or(export_code>0 OR export_code IS NULL) export_errors,
 
-
-CREATE OR REPLACE TRIGGER edit_resolutions_view_trigger
-    INSTEAD OF INSERT OR DELETE OR UPDATE
-    ON public.view_individual_resolutions
-    FOR EACH ROW
-    EXECUTE FUNCTION public.edit_resolution_view();
-
-
--- Also obsolete
-DROP VIEW IF EXISTS view_grouped_combines;
-CREATE or REPLACE VIEW view_grouped_combines as
-SELECT CONCAT('Tile ',tile,' ', resolution,'m ', datum, ' ', production_branch, '_', utm, hemisphere, ' ', locality) tile_name,
-	request_combine, priority, bool_or(combine_code>0) has_errors, bool_and(combine_end_time>combine_request_time and combine_start_time>=combine_request_time) is_finished, bool_or(combine_start_time>=combine_request_time) has_started, bool_and(combine_start_time<combine_request_time) is_waiting,
-	sum((combine_end_time>=combine_start_time and combine_start_time>=combine_request_time)::int) ran, sum((combine_end_time<combine_start_time or combine_start_time<combine_request_time)::int) remaining, sum((combine_end_time<combine_start_time)::int) running,
-	MIN(combine_end_time) age, geometry, tile, utm, datum, resolution, production_branch, hemisphere, locality, res_id, tile_id
+	string_agg(CASE WHEN combine_code>0 THEN CONCAT(combine_warnings_log,'\n') ELSE '' END, '') combine_warnings,
+ 	MIN(combine_end_time) age,
+	request_combine, request_export, geometry
 FROM view_individual_combines
-GROUP BY tile, utm, datum, resolution, production_branch, hemisphere, locality, geometry, request_combine, res_id, tile_id, priority
-ORDER BY production_branch, utm, tile desc;
+GROUP BY tile, utm, datum, production_branch, hemisphere, locality, request_combine, request_export, tile_id, priority, geometry
+ORDER BY production_branch, utm, locality, tile desc;
 
 
-CREATE OR REPLACE FUNCTION edit_view_grouped_combines()
+CREATE OR REPLACE FUNCTION edit_view_tiles()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $function$
    BEGIN
       IF TG_OP = 'UPDATE' THEN
-       UPDATE spec_tiles SET request_combine=NEW.request_combine, request_export=NEW.request_export WHERE t_id=OLD.t_id;
+       UPDATE spec_tiles SET priority=NEW.priority, request_combine=NEW.request_combine, request_export=NEW.request_export WHERE t_id=NEW.t_id;
        RETURN NEW;
       -- ELSIF TG_OP = 'DELETE' THEN
       --  DELETE FROM spec_combines WHERE c_id=OLD.c_id;
@@ -404,8 +393,8 @@ AS $function$
     END;
 $function$;
 
-CREATE OR REPLACE TRIGGER edit_view_grouped_combines_trigger
+CREATE OR REPLACE TRIGGER edit_view_tiles_trigger
     INSTEAD OF DELETE OR UPDATE OR INSERT
-    ON public.view_grouped_combines
+    ON public.view_tiles
     FOR EACH ROW
-    EXECUTE FUNCTION public.edit_view_grouped_combines();
+    EXECUTE FUNCTION public.edit_view_tiles();
